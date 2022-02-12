4c64dcfee38fe3c97d3fd3ea1ad238e3cbe77725
Cheeyoung O <90919724+cheeyoung@users.noreply.github.com>
2022-02-13 02:04:42 +0900

    Create imageTouchpad.js

---
+++ imageTouchpad.js
@@ -0,0 +1,7 @@
+var canvas01 = document.createElement('canvas') ;
+canvas01.id = "image-touchpad" ;
+canvas01.width = 300 ;
+canvas01.height = 200 ;
+
+//
+document.getElementById('section-main').insertBefore(canvas01, document.getElementById('script-touchpad')) ;
