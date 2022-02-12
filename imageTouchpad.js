b37445ee7bab625e8a8203f363d9cf0a62f02000
Cheeyoung O <90919724+cheeyoung@users.noreply.github.com>
2022-02-13 02:30:54 +0900

    Update imageTouchpad.js

--- imageTouchpad.js
+++ imageTouchpad.js
@@ -1,7 +1,18 @@
+// allocation and initialization of the canvas element
 var canvas01 = document.createElement('canvas') ;
 canvas01.id = "image-touchpad" ;
+
+canvas01.addEventListener('touchstart', handleTouchstart, false) ;
+canvas01.addEventListener('touchend', handleTouchstop, false) ;
+canvas01.addEventListener('touchcancel', handleTouchcancel, false) ;
+canvas01.addEventListener('touchmove', handleTouchmove, false) ;
+
+// load an image
 canvas01.width = 300 ;
 canvas01.height = 200 ;
+var ctx01 = canvas01.getContext('2d') ;
+ctx01.fillStyle = 'black' ;
+ctx01.fillRect(0, 0, canvas01.width, canvas01.height) ;
 
-//
+// insert the canvas element to the section(document)
 document.getElementById('section-main').insertBefore(canvas01, document.getElementById('script-touchpad')) ;
